1497c0425389994e5c7360e4438a73ef3426d843
Victoria Ayala <139914452+vimayala@users.noreply.github.com>
2024-01-16 15:12:54 -0800

    w/ assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,7 +6,7 @@ class Play extends Phaser.Scene {
     create() {
         // this.add.text(20, 20, "Rocket Patrol Play")
         // add rectangle with x coord, y coord, width, height, and color (hex) w/ ssetOrigin to adjust rectangle's origin
-        // all defined with variales rather than hardcoded for flexibility
+        // all defined with variables rather than hardcoded for flexibility
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0)
 
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0)
